fbf3f5947e6cb9cf974599991a989c4e80734c85
My Name <rushidilwale19@gmail.com>
2023-05-04 11:52:39 +0530

    navbar.commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Para from './Para'
+const Homepage = () => {
+  return (
+    <>
+    <div>
+        <Navbar/>
+        <Para/>
+    </div>
+    </>
+  )
+}
+
+export default Homepage
